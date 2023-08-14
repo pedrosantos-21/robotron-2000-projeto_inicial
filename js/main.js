a138e5efa0c9bdddf38d22f376a1ee880dced73d
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-08-14 16:45:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,21 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1 
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach((elmento) => {
+    elmento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+        //.textContent -> vai mostrar todo o conteudo do texto.
+    })
 })
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1
-})
+
+function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
