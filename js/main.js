a29627d6fedc2026b5bf0de1635572bc1255c46d
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-08-17 14:16:04 -0300

    Finalizei o projeto

--- js/main.js
+++ js/main.js
@@ -1,22 +1,59 @@
 const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
 
-controle.forEach((elmento) => {
-    elmento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-         //parentNodepara achar o elemnto "pai"
-        
-        //.textContent -> vai mostrar todo o conteudo do texto.
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
-
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
-    
-    if (operacao === "-") {
-        braco.value = parseInt(braco.value) - 1
 
+    if(operacao === "-") {
+        peca.value = parseInd(peca.value) - 1
     } else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatistica(peca) {
+    estatistica.forEach( (elemento ) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
